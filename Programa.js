aeaa311370af7c58e651e6ef69be391e03a8ba50
Yury Marin <yurimar1532@gmail.com>
2023-03-23 19:10:49 -0500

    js

--- Programa.js
+++ Programa.js
@@ -12,18 +12,33 @@ return[area, perimetro];
 }
 
 function calcular_hipotenusa(a, b){
+//Math.pow(a,2)
+const hipotenusa= Math.sqrt(a**2+b**2);
+return hipotenusa;
+}
 
-    //Math.pow(a,2)
-    const hipotenusa= Math.sqrt(a**2+a**2);
-    return hipotenusa;
+function calculadora(a,b){
+
+    document.writeln(a+b);
+    document.writeln(a-b);
+    document.writeln(a*b);
+    document.writeln(a/b);
+
+    console.log(a+b);
+    console.warn(a-b);
+    console.error(a*b);
+    console.info(a/b);
+
+
 
 }
 
+function media(){         
+    
+   let campo_x = document.getElementById('x');
+   let campo_y = document.getElementById('y');
+   let campo_z = document.getElementById('z');
+   console.log(campo_x.value + campo_y.value + campo_z.value )
 
-let resultado= calcular_hipotenusa(10, 20);
-
-
-
-
-
+}
 
