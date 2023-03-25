efeb1245a26ed03ee6d0e33426f17d7ac2d0fa61
Yury Marin <yurimar1532@gmail.com>
2023-03-25 16:14:43 -0500

    V1.0

--- Programa.js
+++ Programa.js
@@ -1,35 +1,3 @@
-function calcular(base, altura){
- 
-    //let para variable de ambito local
-    //var para ambito global
-
-//Constante: Valor que una vez asignado nunca cambia
-const area=base*altura;
-const perimetro=2*(base+altura);
-
-//valores que retorna la funcion a quien la llama
-return[area, perimetro];
-}
-
-function calcular_hipotenusa(a, b){
-//Math.pow(a,2)
-const hipotenusa= Math.sqrt(a**2+b**2);
-return hipotenusa;
-}
-
-function calculadora(a,b){
-
-    document.writeln(a+b);
-    document.writeln(a-b);
-    document.writeln(a*b);
-    document.writeln(a/b);
-
-    console.log(a+b);
-    console.warn(a-b);
-    console.error(a*b);
-    console.info(a/b);
-
-
 
 }
 
